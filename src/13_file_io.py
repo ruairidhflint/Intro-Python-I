43708c89d81062ab886dd0169dca516abb631c50
Rory Flint <roryflintphoto@gmail.com>
2020-02-10 14:09:04 +0000

    Complete File IO section

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -9,11 +9,20 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Print all the contents of the file, then close the file
 # Note: pay close attention to your current directory when trying to open "foo.txt"
 
-# YOUR CODE HERE
+with open('src/foo.txt') as foo:
+    text_doc = foo.read()
+
+print(text_doc)
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
 # then close the file. Open up "bar.txt" and inspect it to make
 # sure that it contains what you expect it to contain
 
-# YOUR CODE HERE
+with open('src/bar.txt', 'w') as bar:
+    new_doc = bar.write('Hello world!\nThis is a test to see if I can open files!\nPretty cool, huh?')
+
+with open('src/bar.txt') as new_bar:
+    my_doc = new_bar.read()
+
+print(my_doc)
