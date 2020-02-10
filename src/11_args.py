13475f0766987e46fe5e454ecbbb6946fe481dcd
Rory Flint <roryflintphoto@gmail.com>
2020-02-10 13:49:31 +0000

    Complete Args section

--- src/11_args.py
+++ src/11_args.py
@@ -4,7 +4,10 @@
 # Write a function f1 that takes two integer positional arguments and returns
 # the sum. This is what you'd consider to be a regular, normal function.
 
-# YOUR CODE HERE
+
+def f1(num1, num2):
+    return num1 + num2
+
 
 print(f1(1, 2))
 
@@ -12,7 +15,13 @@ print(f1(1, 2))
 # sum.
 # Note: Google for "python arbitrary arguments" and look for "*args"
 
-# YOUR CODE HERE
+
+def f2(*nums):
+    if type(nums[0]) is list:
+        return sum(nums[0])
+    else:
+        return sum(nums)
+
 
 print(f2(1))                    # Should print 1
 print(f2(1, 3))                 # Should print 4
@@ -29,7 +38,10 @@ print(f2(a))    # Should print 22
 # arguments.
 # Note: Google "python default arguments" for a hint.
 
-# YOUR CODE HERE
+
+def f3(num1, num2=1):
+    return num1 + num2
+
 
 print(f3(1, 2))  # Should print 3
 print(f3(8))     # Should print 9
@@ -43,7 +55,10 @@ print(f3(8))     # Should print 9
 #
 # Note: Google "python keyword arguments".
 
-# YOUR CODE HERE
+def f4(**kwargs):
+    for k, v in kwargs.items():
+        print(f'key: {str(k)}, value: {str(v)}')
+
 
 # Should print
 # key: a, value: 12
@@ -62,4 +77,4 @@ d = {
 }
 
 # How do you have to modify the f4 call below to make this work?
-f4(d)
+f4(**d)
